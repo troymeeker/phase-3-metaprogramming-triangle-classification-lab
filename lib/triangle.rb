b596ce369b60b177c155261d15c1e365583100bf
troymeeker <troymeeker1@gmail.com>
2021-12-21 11:44:35 -0800

    commit tree d4b0fee3d593a52e1065fa4da82b3a6ffa606f1b

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,40 @@
 class Triangle
   # write code here
+
+  attr_reader :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+     validate_triangle
+     if a == b && b == c 
+       :equilateral
+     elsif a == b || b == c || a == c 
+       :isosceles
+     else
+       :scalene
+     end
+  end
+
+  def sides_must_greater_than_zero?
+    [a,b,c].all?(&:positive?)
+
+  end
+
+  def validate_side_inequality?
+    a + b > c && a + c > b && b + c > a
+
+  end
+
+  def validate_triangle
+    raise TriangleError unless sides_must_greater_than_zero? && validate_side_inequality?
+  end
+
+  class TriangleError < StandardError
+  end
+
 end
